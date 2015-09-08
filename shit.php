fcf3f5a7528b6f05650158855c1d511a25264c91
Matěj Račinský <racinmat@fel.cvut.cz>
2015-09-08 14:04:49 +0200

    Update shit.php

--- shit.php
+++ shit.php
@@ -1,3 +1,4 @@
+//NUM 1
 //in model, eventManager: because filtering in database is too mainstream
 
     /**
@@ -16,7 +17,7 @@
         return $newestEvents;
     }
 
-
+//NUM 2
 //in basePresenter
 	public function createComponent($name) {
 		if ( $name !== 'frame' ) {
@@ -30,3 +31,31 @@
 			return $this->createComponentFrame();
 		}
 	}
+
+//NUM3
+//show articles ordered by newest
+
+//this is good
+    /**
+     * @return array
+     */
+    public function getFrontArticles()
+    {
+        return $this->entityRepository->findBy([], ['createdAt' => 'DESC'], 2);
+    }
+
+
+//but this is not....articles are here shown in inverse order....oh my god, just why....
+
+	{if (count($newestEvents) == 0)}
+		<div class="widget" n:for="$i = 1; $i >= 0; $i--">
+			<h4 class="widget-head">
+				<i class="icon icon-novinka"></i> Článek
+			</h4>
+			<div class="widget-content">
+				<h3><a n:href="Articles:detail, $frontArticles[$i]->id">{$frontArticles[$i]->name}</a></h3>
+                   <p>{$frontArticles[$i]->description}</p>
+				<p class="widget-date"><i class="icon icon-calendar"></i>{$frontArticles[$i]->createdAt|date:"d. m. y"}</p>
+			</div>
+		</div>
+	{/if}
